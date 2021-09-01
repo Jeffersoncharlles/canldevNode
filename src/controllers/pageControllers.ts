ca52c284719ee7087146a68b92b77df217cebd15
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-31 21:02:21 -0300

    add: nav bar dynamic

--- src/controllers/pageControllers.ts
+++ src/controllers/pageControllers.ts
@@ -1,19 +1,51 @@
 import { Request, Response } from "express";
 
+interface Page {
+    banner:{
+        title:string;
+        background:string;
+    };
+}
+
+
 export const home = (req: Request, res: Response)=>{
 
+    let data: Page = {
+        banner:{
+            title:'Todos os animais',
+            background:'allanimals.jpg'
+        },
+    }
 
-    res.render('pages/page');
+    res.render('pages/page',{data});
 }
 export const dogs = (req: Request, res: Response)=>{
+    let data: Page = {
+        banner:{
+            title:'Cachorros',
+            background:'banner_dog.jpg'
+        },
+    }
 
-    //res.render('pages/page');
+    res.render('pages/page',{data});
 }
 export const cats = (req: Request, res: Response)=>{
+    let data: Page = {
+        banner:{
+            title:'Gatos',
+            background:'banner_cat.jpg'
+        },
+    }
 
-    //res.render('pages/page');
+    res.render('pages/page',{data});
 }
 export const fishes = (req: Request, res: Response)=>{
+    let data: Page = {
+        banner:{
+            title:'Peixes',
+            background:'banner_fish.jpg'
+        },
+    }
 
-    //res.render('pages/page');
+    res.render('pages/page',{data});
 }
