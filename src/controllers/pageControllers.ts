f95ed4eec79747f4db42de23d550183616af2c91
Jefferson Charlle <contato@jefferdeveloper.com>
2021-08-31 23:54:57 -0300

    add: active menu

--- src/controllers/pageControllers.ts
+++ src/controllers/pageControllers.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from "express";
+import {createMenuObject} from '../helpers/createMenuObject';
 
 interface Page {
     banner:{
         title:string;
         background:string;
     };
+    menu:{};
 }
 
 
 export const home = (req: Request, res: Response)=>{
 
     let data: Page = {
+        menu:createMenuObject('all'),
         banner:{
             title:'Todos os animais',
             background:'allanimals.jpg'
@@ -21,6 +24,7 @@ export const home = (req: Request, res: Response)=>{
 }
 export const dogs = (req: Request, res: Response)=>{
     let data: Page = {
+        menu:createMenuObject('dog'),
         banner:{
             title:'Cachorros',
             background:'banner_dog.jpg'
@@ -31,6 +35,7 @@ export const dogs = (req: Request, res: Response)=>{
 }
 export const cats = (req: Request, res: Response)=>{
     let data: Page = {
+        menu:createMenuObject('cat'),
         banner:{
             title:'Gatos',
             background:'banner_cat.jpg'
@@ -41,6 +46,7 @@ export const cats = (req: Request, res: Response)=>{
 }
 export const fishes = (req: Request, res: Response)=>{
     let data: Page = {
+        menu:createMenuObject('fish'),
         banner:{
             title:'Peixes',
             background:'banner_fish.jpg'
