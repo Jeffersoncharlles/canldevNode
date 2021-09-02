e83f40109975702026be6b94f674f963d4446ad7
Jefferson Charlle <contato@jefferdeveloper.com>
2021-09-02 13:48:22 -0300

    fix: search redirect

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -11,6 +11,13 @@ export const search = (req: Request, res: Response)=>{
 
     let list = Pet.getFromName(query);
 
+    if (!query) {
+        //uma vez que nao tem o query 
+        // ele redireciona para /
+        //do retorno para parar tudo
+        res.redirect('/');
+        return;
+    }
 
     let data =  {
         menu:createMenuObject(''),
