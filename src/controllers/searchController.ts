b6e012ffd71f7385306a818fd951dc1674e38064
Jefferson Charlle <contato@jefferdeveloper.com>
2021-09-02 13:45:28 -0300

    fix: search value and add 404 page

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import {createMenuObject} from '../helpers/createMenuObject';
+
+import {Pet} from '../models/Pet'
+
+
 
 export const search = (req: Request, res: Response)=>{
+
+    let query: string = req.query.q as string;
+
+    let list = Pet.getFromName(query);
+
+
+    let data =  {
+        menu:createMenuObject(''),
+        list,
+        query
+    }
+
+    res.render('pages/page',{data});
 
 }
